d29b03602051e7a0b79aa36e0dd6f61eaafd873a
HRITWIJ SHRIVASTAVA <hritwij01shrivastava@gmail.com>
2022-11-14 23:01:34 +0530

    first commit

--- backend/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Project\1-Web Development\2-Python\Django\Hillmanwear\1-13-11-2022\hillmanwear\backend\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
